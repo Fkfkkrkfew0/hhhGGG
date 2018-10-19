1ee3c5366e6f1513d316881d29344ed03c87f5d1
Fkfkkrkfew0 <44093001+Fkfkkrkfew0@users.noreply.github.com>
2018-10-19 13:45:22 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,82 +1,9 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
- 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+client.on("ready", () => {
+let channel =     client.channels.get("490591334575636482")
+setInterval(function() {
+channel.send(`lol`);
 });
- 
- 
- 
- 
- 
- 
- 
- 
- 
-
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
-
-
-  const prefix = "!";
-
-client.on("message", message => {
-   
- 
-client.on('ready', () => {
-    setInterval(function(){
-        client.guilds.forEach(g => {
-            if (suck[g.id]) {
-                if (suck[g.id].role) {
-                    var role = g.roles.get(suck[g.id].role);
-                    if (role) {
-                        role.edit({color : "RANDOM"});
-                    };
-                };
-            };
-        });
-    }, 1500);
-})
- 
-client.on("message", message => {
-    if (!message.content.startsWith(prefix)) return;
-    if (message.author.bot) return;
-    if (message.channel.type !== "help") return message.reply("This Command Is Only Allowed In Servers");
-    var args = message.content.split(" ");
-    var command = args[0].slice(prefix.length);
-    switch(command) {
-        case "rainbow" :
-        if (message.member.hasPermission("MANAGE_ROLES")) return message.reply("no no");
-        message.guild.createRole({name : "rainbow", color : "RANDOM"}).then(r => {
-            r.edit({color : "RANDOm"});
-            suck[message.guild.id] = {role : r.id};
-        });
-    };
 });
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
  
  
  
